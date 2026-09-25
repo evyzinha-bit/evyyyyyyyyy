9438cb448ec5e45a47d206392eb6ab3c10c30c7b
evyzinha-bit <evillyn.walsak@escola.pr.gov.br>
2026-09-25 08:26:33 -0300

    Add files via upload

---
+++ evyyyyyyy/js/perguntas.js
@@ -0,0 +1,67 @@
+export const perguntas = [
+    {
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: "No início, ficou assustado com a rapidez da evolução tecnológica."
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: "Ficou entusiasmado com as infinitas possibilidades da nova tecnologia."
+            }
+        ]
+    },
+    {
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial (IA), uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre ela. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de tecnologia em sala de aula. Qual atitude você toma?",
+        alternativas: [
+            {
+                texto: "Utilizar uma ferramenta de busca na internet que utiliza IA para ajudar a encontrar informações relevantes e explicar numa linguagem simples.",
+                afirmacao: "Aprendeu a usar a IA como uma ferramenta prática para acelerar pesquisas de estudos."
+            },
+            {
+                texto: "Escrever o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                afirmacao: "Preferiu focar no pensamento crítico próprio e na troca de ideias com pessoas reais."
+            }
+        ]
+    },
+    {
+        enunciado: "Após a elaboração do trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nesse debate, como você se posiciona sobre o impacto da IA no futuro do trabalho?",
+        alternativas: [
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendo a importância de proteger os trabalhadores.",
+                afirmacao: "Defendeu a proteção dos empregos tradicionais e a segurança dos trabalhadores."
+            },
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: "Acreditou no surgimento de novas carreiras e no aprimoramento das capacidades humanas através das máquinas."
+            }
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design tradicional como o Paint.",
+                afirmacao: "Valorizou o processo criativo manual e autoral na arte digital."
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: "Explorou a geração de imagens por inteligência artificial para expressar suas ideias de forma rápida."
+            }
+        ]
+    },
+    {
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
+        alternativas: [
+            {
+                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                afirmacao: "Entendeu que a IA precisa do discernimento humano para garantir a precisão das informações."
+            },
+            {
+                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+                afirmacao: "Passou a considerar o processo de elaboração de comandos (prompts) como parte da autoria do trabalho."
+            }
+        ]
+    }
+];
